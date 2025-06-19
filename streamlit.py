7dae0f3d214c8f1180b35b55a08fdadc0712580b
MananGupta711 <manan52208@gmail.com>
2025-06-19 11:08:44 +0530

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -1,28 +1,8 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "17ca7c75",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import streamlit as st\n",
-    "st.write(\"Hello\")\n",
-    "\n",
-    "st.text_input(\"Enter your name:\", key=\"name\")\n",
-    "\n",
-    "st.selectbox(\"jashgdjh\",\"asjhdgdh\",\"asjhdg\")\n",
-    "\n",
-    "st.success(\"cabsvbaf\")"
-   ]
-  }
- ],
- "metadata": {
-  "language_info": {
-   "name": "python"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import streamlit as st
+st.write("Hello")
+
+st.text_input("Enter your name:", key="name")
+
+st.selectbox("jashgdjh","asjhdgdh","asjhdg")
+
+st.success("cabsvbaf")
